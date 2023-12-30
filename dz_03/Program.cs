2401ae33eb4c63f1c67233b6347d03e2971dc50e
Artyom Karpenko <azkarpenko@cloud.ru>
2023-12-30 17:25:39 +0300

    ДЗ №3 - вывод наибольшей цифры числа

---
+++ dz_03/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 3: Напишите программу, которая принимает
+// на вход целое число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+Console.Write("Введите число от 10 до 99: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 10 && number <= 99)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;    
+    // Console.Write($"числа {firstDigit} и {secondDigit}");
+    if (firstDigit > secondDigit)
+    {
+        Console.Write($"Наибольшая цифра числа первая {firstDigit}");
+    }
+    else if (firstDigit == secondDigit)
+    {
+         Console.Write($"Обе цифры в числе одинаковые");       
+    }
+    else
+    {
+        Console.Write($"Наибольшая цифра числа вторая {secondDigit}");
+    }
+}
+else
+{
+    Console.Write("Ошибка! Нужно ввести число от 10 до 99!!!");
+}
