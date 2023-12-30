d3a92b44f03cb3b8c127741e5f9d36e3a0405d65
Artyom Karpenko <azkarpenko@cloud.ru>
2023-12-30 14:45:45 +0300

    Таск №2 - принимает на вход трёхзначное число и возводит вторую цифру этого числа в степень, равную третьей цифре

---
+++ Task02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// && - и 
+// || - или 
+if (number >= 100 && number <= 999)
+{
+    int secondDigit = number % 100 / 10;
+    int thirdDigit = number % 10;
+    //Math.Pow(5, 2) => 5 во 2 степени = 25
+    int result = (int)Math.Pow(secondDigit, thirdDigit);
+    Console.WriteLine($"{number}: {secondDigit}^{thirdDigit} = {result}");
+}
+else // Число не трехзначное
+{
+    Console.WriteLine($"Число не трехзначное!");
+}
