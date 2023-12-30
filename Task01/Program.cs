c595ee015f51725c550ee757a34be034f8eeb629
Artyom Karpenko <azkarpenko@cloud.ru>
2023-12-30 14:30:20 +0300

    Таск №1 - удаляем из трехзначного числа вторую цифру

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+//
+// Изначально 100 минимальное трехзначное число и 999 максимальное
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// && - и 
+// || - или 
+if (number >= 100 && number <= 999)
+{
+    int firstDigit = number / 100; // 256 / 100 = 2
+    int thirdDigit = number % 10; // 256 % 10 = 6
+    int result = firstDigit * 10 + thirdDigit * 1;
+    Console.WriteLine(); // number = 256 => 26
+    // Интерполяция - $"Текст {выражение} "
+    Console.WriteLine($"number = {number} => {result}");
+}
+else // Число не трехзначное
+{
+    Console.WriteLine($"Число не трехзначное!");  
+}
