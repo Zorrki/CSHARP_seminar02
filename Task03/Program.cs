da87b6173c0d0c56e4b2a6d2fb4eeb38683df5cd
Artyom Karpenko <azkarpenko@cloud.ru>
2023-12-30 15:14:31 +0300

    Таск №3 - выводим третью цифру слева от заданного числа

---
+++ Task03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру слева
+// от заданного числа
+// 1234567 -> 3
+// 7890 -> 3
+// 123 -> 3
+// 91 -> третьей цифры нет
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int copyNumber = number;
+if (number >= 100)  // Число минимум трехзначное
+{
+    while (number >= 1000) // делим пока число имеет больше 3х знаков
+    {
+        number /= 10; // number = number / 10
+    }
+    int thirdDigit = number % 10;
+    Console.WriteLine($"Третья цифра от числа {copyNumber} = {thirdDigit}");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное!");
+}
